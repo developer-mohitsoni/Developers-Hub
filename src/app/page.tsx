c51ef10632a9c83cf1ba262475beb296c2aa13cb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-22 19:51:02 +0530

    Add DarkAndLightBtn component and use it in the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import DarkAndLightBtn from "@/components/DarkAndLightBtn";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full bg-sone-100 p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all">
@@ -5,7 +7,7 @@ export default function Home() {
       <div className="border mx-auto flex w-full max-w-[600px] flex-col gap-8 rounded p-2">
         <section className="flex justify-between gap-3">
           <p className="text-xl font-semibold">devfinder</p>
-          <p>Light</p>
+          <DarkAndLightBtn />
         </section>
       </div>
     </div>
